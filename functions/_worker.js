dc16548ec6a0520661ca54335bebc974c9265632
Feixu <ivanfeixu@github.com>
2026-05-03 19:39:25 +0800

    fix: 删除 _worker.js，让文件路由正常工作
    - 删除 functions/_worker.js（干扰文件路由）
    - Cloudflare Pages 现在会正确使用 functions/api/*.js 和 functions/api/fund/history.js

--- functions/_worker.js
+++
@@ -1,5 +0,0 @@
-export { onRequest } from './api/ping.js';
-export { onRequest } from './api/fund.js';
-export { onRequest } from './api/funds.js';
-export { onRequest } from './api/fund-history.js';
-export { onRequest } from './api/diag.js';
